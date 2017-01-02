e9677718965c5342cc5ae27bc9f1013e8864afb2
Apollo Tang <tang.apollo@gmail.com>
2017-01-02 10:01:24 -0500

    08 webpack analytics

---
+++ 08-b-webpack-analytics--w-common-chunk/src/main.js
@@ -0,0 +1,12 @@
+if (process && process.env && process.env.CONSOLE_LOG) {
+    console.info('log from file: src/main.js'); // eslint-disable-line no-console
+}
+
+var helper = require('./util/helper.js');
+var thirdPartyCode = require('./vendor/third-party-code.js');
+
+if (process && process.env && process.env.CONSOLE_LOG) {
+    console.info('main.js: thirdPartyCode.name: ', thirdPartyCode.name); // eslint-disable-line no-console
+    console.info('main.js: helper.doHeavyLifting(): ', helper.doHeavyLifting()); // eslint-disable-line no-console
+}
+
