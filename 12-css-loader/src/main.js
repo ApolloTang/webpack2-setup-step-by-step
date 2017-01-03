e2636bb4b3833169f4241e021ee6f25e257a2738
Apollo Tang <tang.apollo@gmail.com>
2017-01-03 10:45:57 -0500

    added css loader

---
+++ 12-css-loader/src/main.js
@@ -0,0 +1,25 @@
+if (process && process.env && process.env.CONSOLE_LOG) {
+    console.info('log from file: src/main.js'); // eslint-disable-line no-console
+}
+
+var helper = require('./util/helper.js');
+var thirdPartyCode = require('./vendor/third-party-code.js');
+
+if (process && process.env && process.env.CONSOLE_LOG) {
+    console.info('main.js: thirdPartyCode.name: ', thirdPartyCode.name); // eslint-disable-line no-console
+    console.info('main.js: helper.doHeavyLifting(): ', helper.doHeavyLifting()); // eslint-disable-line no-console
+}
+
+// ================
+// 11-modules
+// ================
+var elem_twitterImg = require('./modules/twitter');
+document.getElementById("app-container").appendChild(elem_twitterImg);
+
+
+// ================
+// 12-css-loader
+// ================
+require('./main.css');
+
+
