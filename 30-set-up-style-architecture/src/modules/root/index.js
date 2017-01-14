2c1f26a0db8458d1d2d1df0c05bb0b0b4d04067f
Apollo Tang <tang.apollo@gmail.com>
2017-01-13 23:56:27 -0500

    30 style architecture

---
+++ 30-set-up-style-architecture/src/modules/root/index.js
@@ -0,0 +1,44 @@
+if (process && process.env && process.env.CONSOLE_LOG) {
+    console.info('log from file: modules/index/index.js'); // eslint-disable-line no-console
+}
+
+
+import style from './style';
+
+
+const root = document.createElement('div');
+root.className = 'root '+style['module-style'];
+
+
+const btn = document.createElement("button");
+btn.innerHTML = 'click to lazy load';
+root.appendChild(btn);
+
+
+const lazyLoadContainer = document.createElement('div');
+lazyLoadContainer.className = 'lazy-load-container';
+root.appendChild(lazyLoadContainer);
+
+
+let module_twitter = null;
+btn.addEventListener('click', () => {
+    if (module_twitter === null) {
+        console.log('Lazy load modules/twitter...');
+        System.import('modules/twitter').then( ({default:elem_twitterImg}) => {
+            console.log('Loaded item is: ', elem_twitterImg);
+            module_twitter = elem_twitterImg;
+            lazyLoadContainer.appendChild(module_twitter);
+        });
+    }
+});
+
+
+import moduleA from 'modules/module-a';
+root.appendChild(moduleA);
+
+
+import fontTest from 'modules/font-test';
+root.appendChild(fontTest);
+
+export default root;
+
