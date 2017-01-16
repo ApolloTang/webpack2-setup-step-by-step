567b1abf2ab4f4e5ed82b5c70c1155e218dfc6ba
Apollo Tang <tang.apollo@gmail.com>
2017-01-16 01:32:58 -0500

    remove unused code

--- 31-react/src/main.js
+++ 31-react/src/main.js
@@ -12,33 +12,16 @@ console.log('thirdPartyCode.name: ', thirdPartyCode.name); // eslint-disable-lin
 
 
 import style from 'main.less';
-
 import fonts from 'common/fonts';
+
 
 const appContainer = document.getElementById("app-container");
 import global_style from './main';
 appContainer.className = 'appContainer';
 
-// import root from './modules/root';
-// appContainer.appendChild(root);
 
-
-// import React, {Component} from 'react';
-// class Root extends Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//     render() {
-//         return(
-//             <div>
-//                 <p>This is root</p>
-//             </div>
-//         )
-//     }
-// }
-
-import React, {Component} from 'react';
-import Root from './modules/root';
-console.log('Root: ', Root);
+import React from 'react';
 import {render} from 'react-dom';
+import Root from './modules/root';
+
 render( <Root />, appContainer);
