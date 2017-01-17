faff3f755e9117ea6130c84506a21f41ceb7ed7b
Apollo Tang <tang.apollo@gmail.com>
2017-01-16 23:43:39 -0500

    remove debug folder for 31 and 32

--- 31-react-not-watching-file/src/vendor/third-party-code.js
+++
@@ -1,8 +0,0 @@
-if (process && process.env && process.env.CONSOLE_LOG) {
-    console.info('log from file: src/vendor/third-party-code.js'); // eslint-disable-line no-console
-}
-
-const thirdPartyCode = { name:'thirdPartyCode' };
-
-export default thirdPartyCode;
-
